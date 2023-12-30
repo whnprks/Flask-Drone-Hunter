4267823dfc0a94e138d387ee06ffe84c140f90cf
whnprks <wihanperkasa@gmail.com>
2023-12-30 12:13:23 +0700

    test deploy

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='192.168.0.1', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     # app.run(debug=True)
     
